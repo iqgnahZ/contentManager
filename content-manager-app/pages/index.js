c5c12f18231c3a3d5d7a1b691d34a26a5ffea051
LAPTOP-TAFI79L4\Lenovo <zwan0348@student.monash.edu>
2024-05-20 20:46:41 +0800

    comp test

--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -1,6 +1,47 @@
+import React from 'react'
+
+function CompA() {
+  // return (
+  //   <div>
+  //     <h1>CompA</h1>
+  //     <p>Hello Comp A</p>
+  //     <CompB />
+  //   </div>
+  // )
+
+  return(
+    React.createElement('div',null,
+      React.createElement('h1',null,'CompA'),
+      React.createElement('p',null,'Hello Comp A'),
+      React.createElement(CompB)
+    )
+  )
+}
+
+function CompB() {
+  return (
+    <>
+      <h1>CompB</h1>
+      <p>Hello Comp B</p>
+    </>
+  )
+}
+
+class CompC extends React.Component {
+  render() {
+    return(
+      <h1>CompC</h1>
+    )
+  }
+}
+
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <CompA />
+      <CompC />
+    </>
   );
 }

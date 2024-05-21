c943098339a696e1763790cf3874cc83d81c6fa1
LAPTOP-TAFI79L4\Lenovo <zwan0348@student.monash.edu>
2024-05-21 22:54:28 +0800

    state test

--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -1,29 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function CompA() {
   // return (
   //   <div>
   //     <h1>CompA</h1>
   //     <p>Hello Comp A</p>
-  //     <CompB />
   //   </div>
   // )
 
   return(
     React.createElement('div',null,
       React.createElement('h1',null,'CompA'),
-      React.createElement('p',null,'Hello Comp A'),
-      React.createElement(CompB)
+      React.createElement('p',null,'Hello Comp A')
     )
-  )
-}
-
-function CompB() {
-  return (
-    <>
-      <h1>CompB</h1>
-      <p>Hello Comp B</p>
-    </>
   )
 }
 
@@ -37,11 +26,21 @@ class CompC extends React.Component {
 
 
 export default function Home() {
+  const [value, setValue] = useState(10)
+
+  // const increment = () => {
+  //   setValue(value + 1)
+  // }
+
+  // const decrement = () => {
+  //   setValue(value - 1)
+  // }
+
   return (
     <>
-      <h1>Hello World</h1>
-      <CompA />
-      <CompC />
+      Current Value: <h1>{ value }</h1>
+      <button onClick={() => {setValue(value - 1)}}>-</button>
+      <button onClick={() => {setValue(value + 1)}}>+</button>
     </>
   );
 }

8b4c184d7f8ebf626ad419b88eb264d354201a62
LAPTOP-TAFI79L4\Lenovo <zwan0348@student.monash.edu>
2024-05-22 16:07:23 +0800

    Basic Layout

--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -1,46 +1,18 @@
-import React, {useState} from 'react'
-
-function CompA() {
-  // return (
-  //   <div>
-  //     <h1>CompA</h1>
-  //     <p>Hello Comp A</p>
-  //   </div>
-  // )
-
-  return(
-    React.createElement('div',null,
-      React.createElement('h1',null,'CompA'),
-      React.createElement('p',null,'Hello Comp A')
-    )
-  )
-}
-
-class CompC extends React.Component {
-  render() {
-    return(
-      <h1>CompC</h1>
-    )
-  }
-}
+import Newsletter from "@/components/Newsletter";
+import Layout from "@/components/Layout";
+import ResourceHightlight from "@/components/ResourceHightlight";
+import ResourceList from "@/components/ResourceList";
+import Footer from "@/components/Footer";
 
 
 export default function Home() {
-  const [value, setValue] = useState(10)
-
-  // const increment = () => {
-  //   setValue(value + 1)
-  // }
-
-  // const decrement = () => {
-  //   setValue(value - 1)
-  // }
 
   return (
-    <>
-      Current Value: <h1>{ value }</h1>
-      <button onClick={() => {setValue(value - 1)}}>-</button>
-      <button onClick={() => {setValue(value + 1)}}>+</button>
-    </>
+    <Layout>
+      <ResourceHightlight />
+      <Newsletter />
+      <ResourceList />
+      <Footer />
+    </Layout>
   );
 }

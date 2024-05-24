fcba26575dc81312e269ce4800bbb34047c9242a
LAPTOP-TAFI79L4\Lenovo <zwan0348@student.monash.edu>
2024-05-24 18:41:39 +0800

    resources and api

--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -3,16 +3,50 @@ import Layout from "@/components/Layout";
 import ResourceHightlight from "@/components/ResourceHightlight";
 import ResourceList from "@/components/ResourceList";
 import Footer from "@/components/Footer";
+import resources from "./api/resources";
 
 
-export default function Home() {
+function Home({resources}) {
 
   return (
     <Layout>
-      <ResourceHightlight />
+      <ResourceHightlight 
+        resources = {resources.slice(0,2)}
+      />
       <Newsletter />
-      <ResourceList />
+      <ResourceList 
+        resources = {resources.slice(2)}
+      />
       <Footer />
     </Layout>
   );
 }
+
+// it is called every time you will visit the page
+// function is executed on the server
+export async function getServerSideProps() {
+  
+  const resData = await fetch("http://localhost:3000/api/resources")
+  const data = await resData.json()
+
+  return {
+    props: {
+      resources: data
+    }
+  }
+}
+
+// it is called at the build time, and it is callled only once
+// export async function getStaticProps() {
+  
+//   const resData = await fetch("http://localhost:3000/api/resources")
+//   const data = await resData.json()
+
+//   return {
+//     props: {
+//       resources: data
+//     }
+//   }
+// }
+
+export default Home;

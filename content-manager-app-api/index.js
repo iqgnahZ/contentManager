04f1a165b77b9098dfbce1ed49a7ef4ded56ea66
LAPTOP-TAFI79L4\Lenovo <zwan0348@student.monash.edu>
2024-05-30 17:21:19 +0800

    post request redirect

--- content-manager-app-api/index.js
+++ content-manager-app-api/index.js
@@ -8,6 +8,8 @@ const pathToFile = path.resolve('./data.json')
 
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
@@ -15,6 +17,24 @@ app.get('/', (req, res) => {
 app.get('/api/resources', (req, res) => {
   const resources = getResources()
   res.send(resources)
+})
+
+app.post('/api/resources', (req, res) => {
+  const resources = getResources()
+  const resource = req.body
+
+  resource.createdAt = new Date()
+  resource.status = 'inactive'
+  resource.id = Date.now().toString()
+  resources.unshift(resource)
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send('Cannot store data in the file')
+    }
+
+    return res.send('Data has been saved')
+  })
 })
 
 app.listen(PORT, () => {
--- content-manager-app/pages/api/resources.js
+++ content-manager-app/pages/api/resources.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export default async function(req, res){
   if (req.method === "GET") {
   const dataRes = await fetch("http://localhost:3001/api/resources")
@@ -9,8 +11,14 @@ export default async function(req, res){
   if (req.method === "POST") {
     const { title, description, link, priority, timeToFinish } = req.body
     if (!title || !description || !link || !priority || !timeToFinish) {
-      return res.status(402).send("Data are missing!")
+      return res.status(422).send("Data are missing!")
     }
-    return res.send("Data has been received")
+
+    try {
+      const axiosRes = await axios.post("http://localhost:3001/api/resources", req.body)
+      return res.send(axiosRes.data)
+    } catch {
+      return res.status(422).send("Data cannot be stored!")
+    } 
   }
 }
